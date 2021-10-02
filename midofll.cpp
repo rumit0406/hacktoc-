67fcd6e8775a3332be629a5bd72f79e687f6f3e8
nandika2709 <70028281+nandika2709@users.noreply.github.com>
2021-10-03 00:06:38 +0530

    Create midofll.cpp

---
+++ midofll.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* ans=head;
+        while(temp!=NULL and temp->next!=NULL){
+            ans=ans->next;
+            temp=temp->next->next;
+        }
+        return ans;
+    }
+};
